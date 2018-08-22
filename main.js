172f286e78b8406f015b35585360817a1d950a17
ArthurLodbrock <41493045+ArthurLodbrock@users.noreply.github.com>
2018-08-22 23:55:46 +0300

    delete

--- main.js
+++
@@ -1,8 +0,0 @@
-
-
- function Update(){
-  var d = new Date();
-  document.getElementById("time").innerHTML = d.toLocaleTimeString();
-  document.getElementById("date").innerHTML = d.toLocaleDateString();
-  
-}
